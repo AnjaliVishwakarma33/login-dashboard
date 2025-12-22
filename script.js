daa877bacc97b769d687ddaef73f0e376949fc79
AnjaliVishwakarma33 <anjalivishwakarma20112001@gmail.com>
2025-12-22 16:18:21 +0530

    vspagy project

---
+++ script.js
@@ -0,0 +1,53 @@
+function showOTP() {
+  document.getElementById("mobileSection").classList.add("d-none");
+  document.getElementById("otpSection").classList.remove("d-none");
+}
+
+function editNumber() {
+  document.getElementById("otpSection").classList.add("d-none");
+  document.getElementById("mobileSection").classList.remove("d-none");
+}
+
+/* Auto focus OTP inputs */
+document.querySelectorAll(".otp-box input").forEach((input, index, arr) => {
+  input.addEventListener("input", () => {
+    if (input.value && arr[index + 1]) {
+      arr[index + 1].focus();
+    }
+  });
+});
+// advisor detail
+
+document.querySelectorAll(".mobile-input").forEach((input) => {
+  input.addEventListener("input", function () {
+    // Remove non-numeric characters
+    this.value = this.value.replace(/\D/g, "");
+
+    // Limit to 10 digits
+    if (this.value.length > 10) {
+      this.value = this.value.slice(0, 10);
+    }
+
+    const icon = this.parentElement.querySelector("i");
+
+    // Validation: exactly 10 digits
+    if (this.value.length === 10) {
+      this.classList.remove("is-invalid");
+      this.classList.add("is-valid");
+      icon.className = "bi bi-check-circle-fill text-success";
+    } else {
+      this.classList.remove("is-valid");
+      this.classList.add("is-invalid");
+      icon.className = "bi bi-exclamation-circle-fill text-danger";
+    }
+  });
+});
+// dashboard search
+
+function searchAdvisor() {
+  const value = searchInput.value.toLowerCase();
+  document.querySelectorAll(".advisor-item").forEach((card) => {
+    const name = card.querySelector(".advisor-name").innerText.toLowerCase();
+    card.style.display = name.includes(value) ? "block" : "none";
+  });
+}
